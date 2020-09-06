ec3ca629b4d9a67f49b5b8ed582a6f6902bed2be
admin <admin@dg-6780.cnac.local>
2020-09-06 14:49:34 +0100

    overview of the final version

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,68 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
 
+  Card listItem(i, t) {
+    return Card(
+      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      color: Colors.white,
+      child: ListTile(
+        leading: Icon(
+          i,
+          color: Color(0XFF0366d6),
+        ),
+        title: Text(
+          t,
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MiCard',
       home: Scaffold(
         backgroundColor: Color(0XFF0366d6),
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('assets/images/amine.jpg'),
+              ),
+              Text(
+                "Mebrouki Amine",
+                style: TextStyle(
+                  fontSize: 28,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                "FullStack Developper",
+                style: TextStyle(
+                    color: Colors.cyanAccent,
+                    fontSize: 16,
+                    letterSpacing: 3.5,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.cyan,
+                ),
+              ),
+              listItem(Icons.phone, "+213 797 454 539"),
+              listItem(Icons.email, "enimablack@gmail.com"),
+              listItem(Icons.alternate_email, 'DaProduction')
+            ],
+          ),
+        ),
       ),
     );
   }
