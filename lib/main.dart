9315eb6dc1d8edf1be4d0681ab89abfc19273a7d
admin <admin@dg-6780.cnac.local>
2020-09-06 14:47:51 +0100

    Blanc project strated point

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MiCard',
+      home: Scaffold(
+        backgroundColor: Color(0XFF0366d6),
+      ),
+    );
+  }
+}
